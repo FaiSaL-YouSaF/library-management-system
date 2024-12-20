0151f770e1a5ff37c44137b6afe066d3493dfe3e
FaiSaL-YouSaF <faisalyousaf777@gmail.com>
2024-12-20 10:56:33 +0500

    chore: update LibraryCardFactory

--- user-service/src/main/java/com/faisalyousaf777/factory/LibraryCardFactory.java
+++ user-service/src/main/java/com/faisalyousaf777/factory/LibraryCardFactory.java
@@ -1,8 +1,11 @@
 package com.faisalyousaf777.factory;
 
 import com.faisalyousaf777.entity.LibraryCard;
+import com.faisalyousaf777.enums.CardStatus;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class LibraryCardFactory {
@@ -10,16 +13,17 @@ public class LibraryCardFactory {
     public static LibraryCard createLibraryCard() {
         LibraryCard libraryCard = new LibraryCard();
         libraryCard.setCardNumber(generateCardNumber());
-        libraryCard.setIssueDate(java.time.LocalDate.now());
-        libraryCard.setExpiryDate(java.time.LocalDate.now().plusYears(2));
-        libraryCard.setStatus(com.faisalyousaf777.enums.CardStatus.ACTIVE);
+        libraryCard.setIssueDate(LocalDate.now());
+        libraryCard.setExpiryDate(LocalDate.now().plusYears(2));
+        libraryCard.setStatus(CardStatus.ACTIVE);
         libraryCard.setBooksIssued(0);
         libraryCard.setTotalFine(BigDecimal.ZERO);
-        libraryCard.setBooksDueDate(new java.util.HashMap<>());
+        libraryCard.setBooksDueDate(new HashMap<>());
         return libraryCard;
     }
 
     private static String generateCardNumber() {
-        return "LC-" + UUID.randomUUID().toString().substring(0, 5) + "-" + UUID.randomUUID().toString().substring(0, 5);
+        return "LC-" + UUID.randomUUID().toString().substring(0, 5) +
+                "-" + UUID.randomUUID().toString().substring(0, 5);
     }
 }
